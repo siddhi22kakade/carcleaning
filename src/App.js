496ffadac0ee0f045e951be1520f9b56f4217341
siddhi22kakade <kakadesiddhesha803@gmail.com>
2024-10-14 23:58:51 +0530

    new car project by siddhi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Car from './Component/Car.jsx';
+import BookingCar from './Component/BookingCar.jsx';
+function App(){
+  return(
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <>
+    <Car/>
+   {/* <BookingCar/> */}
+
+     </>
   );
 }
-
 export default App;
---
+++ src/User.jsx
@@ -0,0 +1,29 @@
+export const User = [
+    {
+      title: "Basic",
+      price: "₹ 249.00 499",
+      duration: "55min",
+      info: "Floor Mat Cleaning Foam & Pressure Wash Towel Dry Vaccuming"
+    },
+    {
+      title: "Standard",
+      price: "₹ 349.00 599",
+      duration: " 1h 30min",
+      info: "Dashboard Polishing Exterior Vinyl Polish Floor Mat Cleaning Foam & Pressure Wash Glass Cleaning Seat Cleaning Towel Dry Tyre Dressing Vaccuming ."
+    },
+
+    {
+      title: "Premium",
+      price: "₹ 449.00 699",
+      duration: "40min",
+      info: "Dashboard Polishing Floor Mat Cleaning Glass Cleaning Seat Cleaning Vaccuming"
+    },
+    {
+      title: "Luxury",
+      price: "₹ 549.00 799",
+      duration: "45min",
+      info: "Foam & Pressure Wash Glass Cleaning  Towel Dry Tyre Dressing."
+    },
+    
+    
+  ];
